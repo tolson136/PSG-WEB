413648348383208cc4f34a594c36b88c6ef35dec
Peter Olson <polson136@gmail.com>
2018-07-03 11:31:17 -0500

    Add linting

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,31 +3,35 @@ const router = require('express-promise-router')();
 const passport = require('../middleware/passport');
 
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-        try {
-            if (err) {
-                next(err);
-            } else if (!user) {
-                next(new HttpError.Unauthorized());
-            } else {
-                req.login(user, err => {
-                    if (err) {
-                        return next(err);
-                    }
-                    return res.json({
-                        data: user,
-                        metadata: {}
-                    });
-                });
-            }
-        } catch (err) {
-            next(err);
-        }
-    })(req, res, next);
+  passport.authenticate('local', (err, user, info) => {
+    try {
+      if (err) {
+        next(err);
+      } else if (!user) {
+        next(new HttpError.Unauthorized());
+      } else {
+        req.login(user, err => {
+          if (err) {
+            return next(err);
+          }
+          return res.json({
+            data: user,
+            metadata: {}
+          });
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  })(req, res, next);
 });
 
-
-router.post('/logout', function () {
+router.post('/logout', function (req, res) {
+  // @todo: clear session
+  res.json({
+    data: {},
+    metadata: {}
+  });
 });
 
 module.exports = router;
